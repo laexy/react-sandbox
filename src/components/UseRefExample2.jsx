d1d06599902738f716c5b9de0ec5e5d530202f59
laexy <alex31wk@gmail.com>
2022-07-13 14:12:27 +1000

    useRef ex.2 - Getting the previous state

---
+++ src/components/UseRefExample2.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect, useRef } from 'react';
+
+function UseRefExample2() {
+  const [name, setName] = useState('');
+
+  // default value
+  const renders = useRef(1);
+  // getting previous state
+  const prevName = useRef('');
+
+  useEffect(() => {
+    renders.current = renders.current + 1;
+    prevName.current = name;
+  }, [name]);
+
+  return (
+    <div>
+      <h1>Renders: {renders.current}</h1>
+      <h2>Prev Name State: {prevName.current}</h2>
+      <input
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className='form-control mb-3'
+      />
+    </div>
+  );
+}
+
+export default UseRefExample2;
